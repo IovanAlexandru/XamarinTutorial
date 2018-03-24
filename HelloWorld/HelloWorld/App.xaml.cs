7ea311d27d97d8f52c7eb4b8fd9952bbe521e39c
Alexandru Iovan <iovan_alexandru@hotmail.com>
2018-03-24 02:51:09 +0200

    Binding through xaml example

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
 		public App ()
 		{
 			InitializeComponent();
-			MainPage = new OpacityBindingCodePage();
+			MainPage = new OpacityBindingXaml();
 		}
 
 		protected override void OnStart ()
